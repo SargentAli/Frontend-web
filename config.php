0faea9ad2d6f4a76ecf71a846d781b0cdc04630e
SargentAli <95275905+SargentAli@users.noreply.github.com>
2023-01-30 16:17:36 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $currency = 'S$';
 $db_username = 'admin';
 $db_password = 'password';
-$db_name = 'test1';
+$db_name = 'bolt';
 $db_host = 'database-1.cdlocju08nwb.us-east-1.rds.amazonaws.com';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
